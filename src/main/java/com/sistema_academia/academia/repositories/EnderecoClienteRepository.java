f9af3673005150190a828228348eb7b0a11bada7
gbrie <gabriel.alexandre@dcx.ufpb.br>
2023-04-16 11:29:40 -0300

    criação das classes servico e controle de cliente

---
+++ src/main/java/com/sistema_academia/academia/repositories/EnderecoClienteRepository.java
@@ -0,0 +1,11 @@
+package com.sistema_academia.academia.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema_academia.academia.entities.EnderecoCliente;
+
+public interface EnderecoClienteRepository extends JpaRepository<EnderecoCliente,Long>{
+    
+    
+
+}
---
+++ src/main/java/com/sistema_academia/academia/services/ClienteServico.java
@@ -0,0 +1,59 @@
+package com.sistema_academia.academia.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sistema_academia.academia.entities.Cliente;
+import com.sistema_academia.academia.entities.EnderecoCliente;
+import com.sistema_academia.academia.entities.Exercicio;
+import com.sistema_academia.academia.repositories.ClienteRepository;
+import com.sistema_academia.academia.repositories.EnderecoClienteRepository;
+import com.sistema_academia.academia.repositories.ExercicioRepository;
+
+public class ClienteServico {
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ExercicioRepository exercicioRepository;
+
+    @Autowired
+    private EnderecoClienteRepository enderecoRepository;
+
+    public Cliente cadastrarModificar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> findAll(){
+       return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        return clienteRepository.findById(id).get();
+    }
+
+    public Exercicio criarExercicio(Exercicio exercicio){
+        return exercicioRepository.save(exercicio);
+    }
+
+    public List<Exercicio> getExercicios(String cpf){
+
+        Cliente cliente = clienteRepository.findByCpf(cpf);
+        return exercicioRepository.findByCliente(cliente);
+    }
+
+    public EnderecoCliente cadastrarEndereco(EnderecoCliente endereco){
+        return enderecoRepository.save(endereco);
+    }
+
+    public Optional<EnderecoCliente> getEnderecos(Long id){
+
+        return enderecoRepository.findById(id);
+    }
+
+
+
+}
